cbb92e31abe894fba0f5f7667350659ac970791e
jan czaja <elejandc-mini-m1@jans-Mac-mini.Dlink>
2024-05-29 06:35:05 +0200

    change computer

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import AuthProvider from 'react-auth-kit';
+import createStore from 'react-auth-kit/createStore';
+
+const store = createStore({
+    authName:'_auth',
+    authType:'cookie',
+    cookieDomain: window.location.hostname,
+    cookieSecure: false
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <AuthProvider store={store}>
+          <App />
+      </AuthProvider>
   </React.StrictMode>,
 )
